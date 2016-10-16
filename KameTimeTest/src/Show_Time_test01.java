c665cb361fa41e3c361c697f95aaf8d8bcba7671
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-10-17 01:15:02 +0900

    編集
    編集画像がKameTimeTestフォルダのimagesに保存されるように修正

--- KameTimeTest/src/Show_Time_test01.java
+++ KameTimeTest/src/Show_Time_test01.java
@@ -6,7 +6,6 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
 
 public class Show_Time_test01 {
     static{
@@ -20,8 +19,8 @@ public class Show_Time_test01 {
 	   	SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	   	System.out.println("start");
 	   	System.out.println(sdf1.format(date));
-        String path_in = "kame_2592_1944.png";
-        String path_out = sdf1.format(date)+".png";
+        String path_in = "images/kame_2592_1944.png";             //画像を取得
+        String path_out = "images/"+sdf1.format(date)+".png";     //画像を出力時の時刻の名前で保存
 
         Mat mat_src = new Mat();
         Mat mat_dst = new Mat();
@@ -29,7 +28,7 @@ public class Show_Time_test01 {
         mat_src = Highgui.imread(path_in);                         // 入力画像の読み込み
         Core.putText(mat_src, sdf2.format(date), new Point(300, 800), Core.FONT_HERSHEY_SIMPLEX, 1.6f, new Scalar(0, 0, 0), 4);
 
-        Imgproc.cvtColor(mat_src, mat_dst, Imgproc.COLOR_BGR2GRAY); // カラー画像をグレー画像に変換
+        //Imgproc.cvtColor(mat_src, mat_dst, Imgproc.COLOR_BGR2GRAY); // カラー画像をグレー画像に変換
         Highgui.imwrite(path_out, mat_dst);                         // 出力画像を保存
 
         //
