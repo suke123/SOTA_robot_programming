43b87e3f0ed9a417334541e2a693abbeeb9f0d6e
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-10-17 20:04:52 +0900

    編集

--- ShowTime01/src/jp/co/mysota/mymain.java
+++ ShowTime01/src/jp/co/mysota/mymain.java
@@ -5,22 +5,103 @@ import	main.main.GlobalVariable;
 import	jp.vstone.RobotLib.*;
 import	jp.vstone.sotatalk.*;
 import	jp.vstone.sotatalk.SpeechRecog.*;
+import	jp.vstone.camera.*;
+import	java.time.*;
 
 public class mymain
 {
 
-	public mymain()																										//@<BlockInfo>jp.vstone.block.func.constructor,32,32,96,32,False,1,@</BlockInfo>
+	public String time_string;
+	public String date_string;
+	public int getDateTimeElement;
+	public mymain()																										//@<BlockInfo>jp.vstone.block.func.constructor,32,32,288,32,False,4,@</BlockInfo>
 	{
 																														//@<OutputChild>
+		/*String time_string*/;																							//@<BlockInfo>jp.vstone.block.variable,96,32,96,32,False,3,break@</BlockInfo>
+																														//@<EndOfBlock/>
+		/*String date_string*/;																							//@<BlockInfo>jp.vstone.block.variable,160,32,160,32,False,2,break@</BlockInfo>
+																														//@<EndOfBlock/>
+		/*int getDateTimeElement*/;																						//@<BlockInfo>jp.vstone.block.variable,224,32,224,32,False,1,break@</BlockInfo>
+																														//@<EndOfBlock/>
 																														//@</OutputChild>
 	}																													//@<EndOfBlock/>
 
 	//@<Separate/>
-	public void main()																									//@<BlockInfo>jp.vstone.block.func,32,144,272,144,False,2,コメント@</BlockInfo>
+	public void method()																								//@<BlockInfo>jp.vstone.block.func,192,176,288,176,False,5,@</BlockInfo>
 	throws SpeechRecogAbortException {
 		if(!GlobalVariable.TRUE) throw new SpeechRecogAbortException("default");
+
 																														//@<OutputChild>
 																														//@</OutputChild>
+
+	}																													//@<EndOfBlock/>
+
+	//@<Separate/>
+	public void main()																									//@<BlockInfo>jp.vstone.block.func,0,400,976,400,False,17,コメント@</BlockInfo>
+	throws SpeechRecogAbortException {
+		if(!GlobalVariable.TRUE) throw new SpeechRecogAbortException("default");
+
+																														//@<OutputChild>
+		{																												//@<BlockInfo>jp.vstone.block.time.getdatetimeelement,64,400,64,400,False,16,dateTimeに記録された日時情報から、年・月・日・時・分・秒のうち一つをtypeで指定し、変数int getDateTimeElementに代入する@</BlockInfo>
+			LocalDateTime d = (LocalDateTime)localDateTime;
+			getDateTimeElement = d.getHour();
+		}																												//@<EndOfBlock/>
+		if(getDateTimeElement<=11)																						//@<BlockInfo>jp.vstone.block.if,128,304,448,304,False,15,コメント@</BlockInfo>
+		{
+																														//@<OutputChild>
+			GlobalVariable.sotawish.Say((String)"おはよう",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);		//@<BlockInfo>jp.vstone.block.talk.say,256,304,256,304,False,6,@</BlockInfo>
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+		}
+		else if(getDateTimeElement<=16)
+		{
+																														//@<OutputChild>
+			if(getDateTimeElement>11)																					//@<BlockInfo>jp.vstone.block.if,192,352,384,352,False,9,コメント@</BlockInfo>
+			{
+																														//@<OutputChild>
+				GlobalVariable.sotawish.Say((String)"こんにちは",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,320,352,320,352,False,7,@</BlockInfo>
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+			}
+			else
+			{
+																														//@<OutputChild>
+				GlobalVariable.sotawish.Say((String)"こんばんは",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,256,448,256,448,False,8,@</BlockInfo>
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+			}
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+		}
+		else
+		{
+																														//@<OutputChild>
+			GlobalVariable.sotawish.Say((String)"こんばんは",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);		//@<BlockInfo>jp.vstone.block.talk.say,320,496,320,496,False,10,@</BlockInfo>
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+		}
+																														//@<EndOfBlock/>
+		GlobalVariable.sotawish.Say((String)"突然だけど、写真撮るね。5、4、3、2、1、",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,512,400,512,400,False,14,@</BlockInfo>
+																														//@<EndOfBlock/>
+		date_string = CRobotUtil.getDateString();																		//@<BlockInfo>jp.vstone.block.time.getdate,576,400,576,400,False,13,ロボット内のカレンダーより現在年月日を文字列で取得し、変数String date_stringに代入。@</BlockInfo>
+																														//@<EndOfBlock/>
+		time_string = CRobotUtil.getTimeString();																		//@<BlockInfo>jp.vstone.block.time.gettime,640,400,640,400,False,12,ロボット内のカレンダーより現在時刻を文字列で取得し、変数String time_stringに代入。@</BlockInfo>
+																														//@<EndOfBlock/>
+		{																												//@<BlockInfo>jp.vstone.block.facedetect.stillpicture,704,400,704,400,False,11,still@</BlockInfo>
+			String filepath = "/var/sota/photo/";
+			filepath += (String)"picture";
+			boolean isTrakcing=GlobalVariable.robocam.isAliveFaceDetectTask();
+			if(isTrakcing) GlobalVariable.robocam.StopFaceTraking();
+			GlobalVariable.robocam.initStill(new CameraCapture(CameraCapture.CAP_IMAGE_SIZE_5Mpixel, CameraCapture.CAP_FORMAT_MJPG));
+			GlobalVariable.robocam.StillPicture(filepath);
+
+			CRobotUtil.Log("stillpicture","save picthre file to \"" + filepath +"\"");
+			if(isTrakcing) GlobalVariable.robocam.StartFaceTraking();
+		}																												//@<EndOfBlock/>
+		GlobalVariable.sotawish.Say((String)"撮影終わったよ。ありがとう。",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,912,400,912,400,False,18,@</BlockInfo>
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+
 	}																													//@<EndOfBlock/>
 
 }
