01ce00197114c05788fed852310f271234c4d36b
Takanori Sukekiyo <14ec066@ms.dendai.ac.jp>
2016-11-10 11:00:10 +0900

    編集

--- ShowTime01/src/jp/co/mysota/time_show.java
+++ ShowTime01/src/jp/co/mysota/time_show.java
@@ -208,12 +208,35 @@ public class time_show
 	}																													//@<EndOfBlock/>
 
 	//@<Separate/>
-	public void photo_time()																							//@<BlockInfo>jp.vstone.block.func,0,784,160,784,False,28,@</BlockInfo>
+	public void method1()																								//@<BlockInfo>jp.vstone.block.func,0,992,208,992,False,28,@</BlockInfo>
 	throws SpeechRecogAbortException {
 		if(!GlobalVariable.TRUE) throw new SpeechRecogAbortException("default");
 
 																														//@<OutputChild>
-		date_string = CRobotUtil.getDateString();																		//@<BlockInfo>jp.vstone.block.freeproc,80,784,80,784,False,27,@</BlockInfo>
+		date_string = CRobotUtil.getDateString();																		//@<BlockInfo>jp.vstone.block.freeproc,96,992,96,992,False,27,@</BlockInfo>
+		time_string = CRobotUtil.getTimeString();
+
+		String filepath = "/var/sota/photo/";
+		filepath += (String)"picture";
+		boolean isTrakcing=GlobalVariable.robocam.isAliveFaceDetectTask();
+		if(isTrakcing) GlobalVariable.robocam.StopFaceTraking();
+		GlobalVariable.robocam.initStill(new CameraCapture(CameraCapture.CAP_IMAGE_SIZE_5Mpixel, CameraCapture.CAP_FORMAT_MJPG));
+		GlobalVariable.robocam.StillPicture(filepath);
+
+		CRobotUtil.Log("stillpicture","save picthre file to \"" + filepath +"\"");
+		if(isTrakcing) GlobalVariable.robocam.StartFaceTraking();
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+
+	}																													//@<EndOfBlock/>
+
+	//@<Separate/>
+	public void photo_time()																							//@<BlockInfo>jp.vstone.block.func,0,784,160,784,False,29,@</BlockInfo>
+	throws SpeechRecogAbortException {
+		if(!GlobalVariable.TRUE) throw new SpeechRecogAbortException("default");
+
+																														//@<OutputChild>
+		date_string = CRobotUtil.getDateString();																		//@<BlockInfo>jp.vstone.block.freeproc,80,784,80,784,False,30,@</BlockInfo>
 		time_string = CRobotUtil.getTimeString();
 
 		String date = date_string;
@@ -260,6 +283,10 @@ public class time_show
 		String dt2 = date1+"_"+time1;
 
 
+		GlobalVariable.sotawish.Say((String)"写真撮るよ",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);
+		GlobalVariable.sotawish.Say((String)"3　",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);
+		GlobalVariable.sotawish.Say((String)"2　",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);
+		GlobalVariable.sotawish.Say((String)"1　",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);
 
 
 		String filepath = "/var/sota/photo/";
@@ -278,32 +305,9 @@ public class time_show
 		Mat mat_src = new Mat();
 
 		mat_src = Imgcodecs.imread(path_in);
-		Imgproc.putText(mat_src, dt, new Point(1650, 1900), Core.FONT_HERSHEY_SIMPLEX, 2.4f, new Scalar(255, 0, 0), 4);
+		Imgproc.putText(mat_src, dt, new Point(1650, 1900), Core.FONT_HERSHEY_SIMPLEX, 2.4f, new Scalar(0, 0, 255), 4);
 
 		Imgcodecs.imwrite(path_out, mat_src);
-																														//@<EndOfBlock/>
-																														//@</OutputChild>
-
-	}																													//@<EndOfBlock/>
-
-	//@<Separate/>
-	public void method1()																								//@<BlockInfo>jp.vstone.block.func,0,992,208,992,False,30,@</BlockInfo>
-	throws SpeechRecogAbortException {
-		if(!GlobalVariable.TRUE) throw new SpeechRecogAbortException("default");
-
-																														//@<OutputChild>
-		date_string = CRobotUtil.getDateString();																		//@<BlockInfo>jp.vstone.block.freeproc,96,992,96,992,False,29,@</BlockInfo>
-		time_string = CRobotUtil.getTimeString();
-
-		String filepath = "/var/sota/photo/";
-		filepath += (String)"picture";
-		boolean isTrakcing=GlobalVariable.robocam.isAliveFaceDetectTask();
-		if(isTrakcing) GlobalVariable.robocam.StopFaceTraking();
-		GlobalVariable.robocam.initStill(new CameraCapture(CameraCapture.CAP_IMAGE_SIZE_5Mpixel, CameraCapture.CAP_FORMAT_MJPG));
-		GlobalVariable.robocam.StillPicture(filepath);
-
-		CRobotUtil.Log("stillpicture","save picthre file to \"" + filepath +"\"");
-		if(isTrakcing) GlobalVariable.robocam.StartFaceTraking();
 																														//@<EndOfBlock/>
 																														//@</OutputChild>
 
